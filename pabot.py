e7a88c950268597e066b423b22c5327d3743ee31
Mikko Korpela <mikko.korpela@gmail.com>
2013-12-18 14:46:11 +0200

    support jybot and process number control

--- pabot.py
+++ pabot.py
@@ -3,6 +3,7 @@ import multiprocessing
 from glob import glob
 from StringIO import StringIO
 import shutil
+import subprocess
 from robot import run, rebot
 from robot.api import ExecutionResult
 from robot.result.visitor import ResultVisitor
@@ -10,8 +11,6 @@ from multiprocessing.pool import Pool
 from tempfile import mkdtemp
 from robot.run import USAGE
 from robot.utils import ArgumentParser
-
-NUMBER_OF_PARALLEL_RUNNERS = max(multiprocessing.cpu_count(), 2)
 
 
 def execute_and_wait(args):
@@ -21,6 +20,16 @@ def execute_and_wait(args):
     if rc != 0:
         print 'EXECUTION FAILED IN %s' % suite_name
 
+def execute_with_java(args):
+    datasources, outs_dir, options, suite_name = args
+    print 'EXECUTING PARALLEL SUITE %s WITH JYBOT' % suite_name
+    cmd = ['jybot']+_options_for_java_executor(options, outs_dir, suite_name) + datasources
+    rc = subprocess.Popen(cmd,
+                          shell=True,
+                          stderr=subprocess.PIPE,
+                          stdout=subprocess.PIPE).wait()
+    if rc != 0:
+        print 'EXECUTION FAILED IN %s' % suite_name
 
 def _options_for_executor(options, outs_dir, suite_name):
     options = options.copy()
@@ -32,6 +41,19 @@ def _options_for_executor(options, outs_dir, suite_name):
     options['stdout'] = StringIO()
     options['stderr'] = StringIO()
     return options
+
+def _options_for_java_executor(*args):
+    opts = _options_for_executor(*args)
+    res = []
+    for k, v in opts.items():
+        if isinstance(v, basestring):
+            res += ['--' + str(k), str(v)]
+        if isinstance(v, bool) and (v is True):
+            res += ['--' + str(k)]
+        if isinstance(v, list):
+            for value in v:
+                res += ['--' + str(k), str(value)]
+    return res
 
 class GatherSuiteNames(ResultVisitor):
 
@@ -54,14 +76,24 @@ def get_suite_names(output_file):
        return []
 
 def get_args():
-    options, datasources = ArgumentParser(USAGE).parse_args(sys.argv[1:])
+    args = sys.argv[1:]
+    pabot_args = {'java':False,
+                  'splits':max(multiprocessing.cpu_count(), 2)}
+    while args and args[0] in ['-J', '-S']:
+        if args[0] == '-J':
+            pabot_args['java'] = True
+            args = args[1:]
+        if args[0] == '-S':
+            pabot_args['splits'] = int(args[1])
+            args = args[2:]
+    options, datasources = ArgumentParser(USAGE).parse_args(args)
     keys = set()
     for k in options:
         if options[k] is None:
             keys.add(k)
     for k in keys:
         del options[k]
-    return options, datasources
+    return options, datasources, pabot_args
 
 def solve_suite_names(outs_dir, datasources, options):
     options = options.copy()
@@ -87,12 +119,12 @@ def _options_for_rebot(options, datasources):
 if __name__ == '__main__':
     outs_dir = mkdtemp()
     try:
-        suite_dir = sys.argv[-1]
-        options, datasources = get_args()
+        options, datasources, pabot_args = get_args()
         suite_names = solve_suite_names(outs_dir, datasources, options)
         if suite_names:
-            process_pool = Pool(NUMBER_OF_PARALLEL_RUNNERS)
-            process_pool.map_async(execute_and_wait, [(datasources, outs_dir, options, suite) for suite in suite_names])
+            process_pool = Pool(pabot_args['splits'])
+            process_pool.map_async(execute_and_wait if not pabot_args['java'] else execute_with_java,
+                                   [(datasources, outs_dir, options, suite) for suite in suite_names])
             process_pool.close()
             process_pool.join()
         sys.exit(rebot(*sorted(glob(os.path.join(outs_dir, '*.xml'))), **_options_for_rebot(options, datasources)))
