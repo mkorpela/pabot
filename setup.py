0ccd054e2fc42a3fd3a0135c311b9fe42c4bd6ed
Mikko Korpela <mikko.korpela@gmail.com>
2014-01-15 13:26:15 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import os
+from setuptools import find_packages
+
+name = 'Mikko Korpela'
+# I might be just a little bit too much afraid of those bots..
+address = name.lower().replace(' ', '.')+chr(64)+'gmail.com'
+
+setup(name='PaBot',
+      version='0.1',
+      description='Parallelizing test runner for Robot Framework',
+      author=name,
+      author_email=address,
+      url='https://github.com/mkorpela/pabot',
+      packages=find_packages(),
+      scripts = [os.path.join('scripts', 'pabot'), os.path.join('scripts', 'pabot.bat')],
+      install_requires = ['robotframework'])
