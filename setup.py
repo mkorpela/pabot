9f16f9a4d60c34a6d591a045d9c95f0b117cc3ab
Mikko Korpela <mikko.korpela@gmail.com>
2014-01-15 15:17:24 +0800

    rename tool

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ name = 'Mikko Korpela'
 # I might be just a little bit too much afraid of those bots..
 address = name.lower().replace(' ', '.')+chr(64)+'gmail.com'
 
-setup(name='PaBot',
+setup(name='robotframework-pabot',
       version='0.1',
       description='Parallelizing test runner for Robot Framework',
       author=name,
