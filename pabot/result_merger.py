25c7f5a764286594b49d38cb3180b5d28401e37c
Mikko Korpela <mikko.korpela@gmail.com>
2014-07-30 16:33:27 +0300

    Re-license Pabot in GPLv3

--- pabot/result_merger.py
+++ pabot/result_merger.py
@@ -1,6 +1,22 @@
-__author__ = 'mkorpela'
-#  Copyright 2014 Mikko Korpela GPLv3
-#  partly based on work by Nokia Solutions and Networks
+#    Copyright 2014 Mikko Korpela
+#
+#    This file is part of Pabot - A parallel executor for Robot Framework test cases..
+#
+#    Pabot is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Pabot is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Pabot.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  partly based on work by Pekka Klarck
+#  by Nokia Solutions and Networks
 #  that was licensed under Apache License Version 2.0
 
 from robot.model import SuiteVisitor
