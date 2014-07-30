d407cc24a9bfaaee3f739a87e1e5a483ccba243d
Mikko Korpela <mikko.korpela@gmail.com>
2014-07-30 20:20:30 +0300

    Update version to 0.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ name = 'Mikko Korpela'
 address = name.lower().replace(' ', '.')+chr(64)+'gmail.com'
 
 setup(name='robotframework-pabot',
-      version='0.6',
+      version='0.7',
       description='Parallel test runner for Robot Framework',
       author=name,
       author_email=address,
