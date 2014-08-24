16fef1eda855de6594a11b613d86374a3032f071
Mikko Korpela <mikko.korpela@gmail.com>
2014-08-24 21:55:45 +0300

    fix license string

--- setup.py
+++ setup.py
@@ -16,5 +16,5 @@ setup(name='robotframework-pabot',
       url='https://github.com/mkorpela/pabot',
       packages=find_packages(),
       scripts = [os.path.join('scripts', 'pabot'), os.path.join('scripts', 'pabot.bat')],
-      license='LICENSE.txt',
+      license='GNU General Public License v3',
       install_requires = ['robotframework'])
