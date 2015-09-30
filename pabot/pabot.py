51b3597c7c136492d70c63689eb5846093d6c7bb
Mikko Korpela <mikko.korpela@gmail.com>
2015-09-30 09:25:28 +0300

    update version to 0.19

--- pabot/pabot.py
+++ pabot/pabot.py
@@ -365,7 +365,7 @@ def main(args):
         else:
             print 'No tests to execute'
     except Information, i:
-        print """A parallel executor for Robot Framework test cases. Version 0.18.
+        print """A parallel executor for Robot Framework test cases. Version 0.19.
 
 Supports all Robot Framework command line options and also following options (these must be before normal RF options):
 
