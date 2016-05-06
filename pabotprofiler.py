177683693f34ec79ae9655b6663b89cb2aaf51a2
Mikko Korpela <mikko.korpela@gmail.com>
2016-05-06 07:39:06 +0300

    Add pabotprofiler for debugging

---
+++ pabotprofiler.py
@@ -0,0 +1,12 @@
+import cProfile
+import pstats
+from pabot.pabot import main
+import os, sys
+import tempfile
+
+profile_results = tempfile.mktemp(suffix='.out', prefix='pybot-profile', dir='.')
+cProfile.run('main(sys.argv[1:])', profile_results)
+stats = pstats.Stats(profile_results)
+stats.sort_stats('cumulative').print_stats(50)
+os.remove(profile_results)
+
