21ffda4b2e77269fbd2845d530b63730043e25b4
Mikko Korpela <mikko.korpela@gmail.com>
2016-05-10 23:45:38 +0300

    fix more lint

--- tests/test_pabot.py
+++ tests/test_pabot.py
@@ -2,18 +2,20 @@ import unittest
 import time
 from pabot import pabot
 
+
 class PabotTests(unittest.TestCase):
 
     def test_parse_args(self):
-        options, datasources, pabot_args = pabot._parse_args(['--command', 'my_own_command.sh', '--end-command',
-                 '--processes', '12',
-                 '--verbose',
-                 '--resourcefile', 'resourcefile.ini',
-                 '--pabotlib',
-                 '--pabotlibhost', '123.123.233.123',
-                 '--pabotlibport', '4562',
-                 '--suitesfrom', 'some.xml',
-                 'suite'])
+        options, datasources, pabot_args = pabot._parse_args(
+            ['--command', 'my_own_command.sh', '--end-command',
+             '--processes', '12',
+             '--verbose',
+             '--resourcefile', 'resourcefile.ini',
+             '--pabotlib',
+             '--pabotlibhost', '123.123.233.123',
+             '--pabotlibport', '4562',
+             '--suitesfrom', 'some.xml',
+             'suite'])
         self.assertEqual(pabot_args['command'], ['my_own_command.sh'])
         self.assertEqual(pabot_args['processes'], 12)
         self.assertEqual(pabot_args['resourcefile'], 'resourcefile.ini')
@@ -23,8 +25,12 @@ class PabotTests(unittest.TestCase):
         self.assertEqual(pabot_args['suitesfrom'], 'some.xml')
         self.assertEqual(datasources, ['suite'])
 
+
     def test_start_and_stop_remote_library(self):
-        lib_process = pabot._start_remote_library({'pabotlib':True, 'pabotlibhost':'127.0.0.1', 'pabotlibport':8270})
+        lib_process = pabot._start_remote_library(
+            {'pabotlib': True,
+             'pabotlibhost': '127.0.0.1',
+             'pabotlibport': 8270})
         self.assertTrue(lib_process.poll() is None)
         time.sleep(0.3)
         pabot._stop_remote_library(lib_process)
