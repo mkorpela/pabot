b207ae6eb4f2972d8e654af09a58025edbeebd1a
Mikko Korpela <mikko.korpela@gmail.com>
2017-10-08 19:26:37 +0300

    StringIO import

--- pabot/pabot.py
+++ pabot/pabot.py
@@ -63,7 +63,10 @@ import datetime
 import multiprocessing
 import uuid
 from glob import glob
-from StringIO import StringIO
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 import shutil
 import subprocess
 import threading
