c4b72e4851e54a7ff6b99a9933bf91ebd4773962
Mikko Korpela <mikko.korpela@gmail.com>
2019-04-21 18:23:10 +0300

    v 0.56

--- pabot/pabot.py
+++ pabot/pabot.py
@@ -16,7 +16,7 @@
 #
 #  partly based on work by Nokia Solutions and Networks Oyj
 """A parallel executor for Robot Framework test cases.
-Version 0.55.
+Version 0.56.
 
 Supports all Robot Framework command line options and also following
 options (these must be before normal RF options):
