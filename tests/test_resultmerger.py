dd257e452af12ccc7ed5e30b332dd4b0b8ae84f5
Mikko Korpela <mikko.korpela@gmail.com>
2019-04-24 18:45:04 +0300

    add test resultmerger file

---
+++ tests/test_resultmerger.py
@@ -0,0 +1,15 @@
+import unittest
+import time
+import os
+import tempfile
+import shutil
+import random
+from pabot import pabotlib
+
+class ResultMergerTests(unittest.TestCase):
+
+    def test_something(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
