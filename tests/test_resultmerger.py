8d27f4d61739b6ce2123f4d9fa1ca773673730c3
Mikko Korpela <mikko.korpela@gmail.com>
2019-04-24 22:47:11 +0300

    test for merging suite level things

--- tests/test_resultmerger.py
+++ tests/test_resultmerger.py
@@ -11,14 +11,18 @@ from robot.result.visitor import ResultVisitor
 class ResultStats(ResultVisitor):
 
     def __init__(self):
+        self.suites = []
         self.tests = []
 
     def end_test(self, test):
         self.tests.append(test.longname)
 
+    def end_suite(self, suite):
+        self.suites.append(suite.longname)
+
 class ResultMergerTests(unittest.TestCase):
 
-    def test_something(self):
+    def test_test_level_run_merge(self):
         result = result_merger.merge([
             'tests/outputs/first.xml',
             'tests/outputs/second.xml',
@@ -27,8 +31,28 @@ class ResultMergerTests(unittest.TestCase):
         visitor = ResultStats()
         result.visit(visitor)
         self.assertEqual(
+            ['Tmp.Tests', 'Tmp'],
+            visitor.suites
+        )
+        self.assertEqual(
             ['Tmp.Tests.First', 'Tmp.Tests.Second', 'Tmp.Tests.Third'], 
             visitor.tests)
+
+    def test_suite_level_run_merge(self):
+        result = result_merger.merge([
+            'tests/outputs/tests.xml',
+            'tests/outputs/tests2.xml'
+        ], {}, "root")
+        visitor = ResultStats()
+        result.visit(visitor)
+        self.assertEqual(
+            ['Tmp.Tests.First', 'Tmp.Tests.Second', 'Tmp.Tests.Third', 
+            'Tmp.Tests2.First 2', 'Tmp.Tests2.Second 2', 'Tmp.Tests2.Third 2'], 
+            visitor.tests)
+        self.assertEqual(
+            ['Tmp.Tests', 'Tmp.Tests2', 'Tmp'],
+            visitor.suites
+        )
 
 
 if __name__ == '__main__':
