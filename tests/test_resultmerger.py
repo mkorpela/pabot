b70ff69fcfe46f120fada4332a100a3f86470fa5
Mikko Korpela <mikko.korpela@gmail.com>
2019-04-24 19:19:48 +0300

    failing test of merging outputs

--- tests/test_resultmerger.py
+++ tests/test_resultmerger.py
@@ -4,12 +4,32 @@ import os
 import tempfile
 import shutil
 import random
-from pabot import pabotlib
+import pabot.result_merger as result_merger
+from robot.result.visitor import ResultVisitor
+
+
+class ResultStats(ResultVisitor):
+
+    def __init__(self):
+        self.tests = []
+
+    def end_test(self, test):
+        self.tests.append(test.longname)
 
 class ResultMergerTests(unittest.TestCase):
 
     def test_something(self):
-        pass
+        result = result_merger.merge([
+            'tests/outputs/first.xml',
+            'tests/outputs/second.xml',
+            'tests/outputs/third.xml'
+        ], {}, "root")
+        visitor = ResultStats()
+        result.visit(visitor)
+        self.assertEqual(
+            ['Tmp.Tests.First', 'Tmp.Tests.Second', 'Tmp.Tests.Third'], 
+            visitor.tests)
+
 
 if __name__ == '__main__':
     unittest.main()
