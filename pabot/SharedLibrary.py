48e9ef7a21ed92bc91238560cad0257ee208b391
Mikko Korpela <mikko.korpela@gmail.com>
2019-09-04 17:07:33 +0300

    almost working with robto

--- pabot/SharedLibrary.py
+++ pabot/SharedLibrary.py
@@ -2,6 +2,7 @@ from robot.libraries.BuiltIn import BuiltIn
 from robot.libraries.Remote import Remote
 from robot.api import logger
 from robot.running.testlibraries import TestLibrary
+from robot.running.context import EXECUTION_CONTEXTS
 from .pabotlib import PABOT_QUEUE_INDEX
 
 class SharedLibrary(object):
@@ -13,6 +14,7 @@ class SharedLibrary(object):
         Import a library so that the library instance is shared between executions.
         [https://pabot.org/PabotLib.html?ref=log#import-shared-library|Open online docs.]
         """
+        self._remote = None
         if BuiltIn().get_variable_value('${%s}' % PABOT_QUEUE_INDEX) is None:
             logger.debug("Not currently running pabot. Importing library for this process.")
             self._lib = TestLibrary(name)
@@ -26,14 +28,19 @@ class SharedLibrary(object):
             except RuntimeError:
                 logger.error('No connection - is pabot called with --pabotlib option?')
                 raise
-            self._lib = Remote("http://127.0.0.1:%s" % port)
+            self._remote = Remote("http://127.0.0.1:%s" % port)
             logger.debug("Lib imported with name %s from http://127.0.0.1:%s" % (name, port))
         else:
             logger.error('No connection - is pabot called with --pabotlib option?')
             raise AssertionError('No connection to pabotlib')
 
     def get_keyword_names(self):
-        return self._lib.get_keyword_names()
+        if self._remote:
+            return self._remote.get_keyword_names()
+        return [handler.name for handler in self._lib.handlers]
 
     def run_keyword(self, name, args, kwargs):
-        return self._lib.run_keyword(name, args, kwargs)
+        if self._remote:
+            return self._remote.run_keyword(name, args, kwargs)
+        print(repr(kwargs))
+        self._lib.handlers[name].create_runner(name)._run(EXECUTION_CONTEXTS.current, args)
