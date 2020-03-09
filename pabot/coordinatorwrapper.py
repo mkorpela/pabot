1626a8ec9c048cd06a2152931f487750da678e5f
Mikko Korpela <mikko.korpela@gmail.com>
2020-03-09 21:16:56 +0200

    wrapper expects 3.6

--- pabot/coordinatorwrapper.py
+++ pabot/coordinatorwrapper.py
@@ -2,6 +2,6 @@ import sys
 try:
     from .py3.coordinator import main
 except SyntaxError:
-    print("Coordinator needs to be run in Python 3")
+    print("Coordinator needs to be run in Python >= 3.6")
     print("You are running %s" % sys.version)
     sys.exit(1)
--- pabot/workerwrapper.py
+++ pabot/workerwrapper.py
@@ -2,6 +2,6 @@ import sys
 try:
     from .py3.worker import main
 except SyntaxError:
-    print("Worker needs to be run in Python 3")
+    print("Worker needs to be run in Python >= 3.6")
     print("You are running %s" % sys.version)
     sys.exit(1)
