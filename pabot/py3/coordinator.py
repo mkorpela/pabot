216a6e0eb08eb5e52f5350b0fe5f4beb4530aea4
Mikko Korpela <mikko.korpela@gmail.com>
2020-03-09 21:46:12 +0200

    some typing

--- pabot/py3/coordinator.py
+++ pabot/py3/coordinator.py
@@ -1,7 +1,9 @@
 import asyncio
 import websockets
+from websockets.server import WebSocketServerProtocol
 
-async def echo(websocket, path):
+async def echo(websocket: WebSocketServerProtocol, path: str):
+    message: object
     async for message in websocket:
         print(f'echoing message {message}')
         await websocket.send(message)
@@ -9,6 +11,7 @@ async def echo(websocket, path):
 def main(args=None):
     start_server = websockets.serve(echo, "localhost", 8765)
     asyncio.get_event_loop().run_until_complete(start_server)
+    print(f'Coordinator started at localhost:8765')
     asyncio.get_event_loop().run_forever()
 
 if __name__ == '__main__':
