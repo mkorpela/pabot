cef38438b6031db6fb520cd471e6d7ac0d1c5a81
Mikko Korpela <mikko.korpela@gmail.com>
2020-05-15 00:00:43 +0300

    typing arguments

--- pabot/arguments.py
+++ pabot/arguments.py
@@ -1,6 +1,6 @@
 import multiprocessing
 import re
-from typing import Optional, List, Dict
+from typing import Optional, List, Dict, Tuple
 
 from robot import __version__ as ROBOT_VERSION
 from robot.errors import DataError
@@ -13,14 +13,14 @@ from pabot.execution_items import SuiteItem, TestItem, IncludeItem, DynamicTestI
 ARGSMATCHER = re.compile(r'--argumentfile(\d+)')
 
 
-def _processes_count():
+def _processes_count():  # type: () -> int
     try:
         return max(multiprocessing.cpu_count(), 2)
     except NotImplementedError:
         return 2
 
 
-def parse_args(args):
+def parse_args(args):  # type: (List[str]) -> Tuple[Dict[str, object], List[str], Dict[str, object], Dict[str, object]]
     pabot_args = {'command': ['pybot' if ROBOT_VERSION < '3.1' else 'robot'],
                   'verbose': False,
                   'help': False,
@@ -123,12 +123,12 @@ def parse_args(args):
     if len(datasources) > 1 and options['name'] is None:
         options['name'] = 'Suites'
         options_for_subprocesses['name'] = 'Suites'
-    _delete_none_keys(options)
-    _delete_none_keys(options_for_subprocesses)
-    return options, datasources, pabot_args, options_for_subprocesses
+    opts = _delete_none_keys(options)
+    opts_sub = _delete_none_keys(options_for_subprocesses)
+    return opts, datasources, pabot_args, opts_sub
 
 
-def _parse_ordering(filename): # type: (str) -> Optional[List[ExecutionItem]]
+def _parse_ordering(filename):  # type: (str) -> List[ExecutionItem]
     try:
         with open(filename, "r") as orderingfile:
             return [parse_execution_item_line(line.strip()) for line in orderingfile.readlines()]
@@ -136,18 +136,17 @@ def _parse_ordering(filename): # type: (str) -> Optional[List[ExecutionItem]]
         raise DataError("Error parsing ordering file '%s'" % filename)
 
 
-def _delete_none_keys(d):
-    # type: (Dict[str, Optional[object]]) -> None
+def _delete_none_keys(d):  # type: (Dict[str, Optional[object]]) -> Dict[str, object]
     keys = set()
     for k in d:
         if d[k] is None:
             keys.add(k)
     for k in keys:
         del d[k]
+    return d
 
 
-def parse_execution_item_line(text):
-    # type: (str) -> ExecutionItem
+def parse_execution_item_line(text):  # type: (str) -> ExecutionItem
     if text.startswith('--suite '):
         return SuiteItem(text[8:])
     if text.startswith('--test '):
