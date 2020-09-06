045d89ebdfb6105f08b9f61e0a224d5235de353d
Mikko Korpela <mikko.korpela@gmail.com>
2020-09-06 22:05:16 +0300

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ name = 'Mikko Korpela'
 address = name.lower().replace(' ', '.')+chr(64)+'gmail.com'
 
 setup(name='robotframework-pabot',
-      version='1.9.0',
+      version='1.9.1',
       description='Parallel test runner for Robot Framework',
       long_description='A parallel executor for Robot Framework tests.'
                        ' With Pabot you can split one execution into multiple and save test execution time.',
