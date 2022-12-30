27c64bfd28870e863567d4d47f7a4da62c65ecde
Mikko Korpela <mikko.korpela@gmail.com>
2022-12-30 12:52:00 +0200

    more is more

--- setup.py
+++ setup.py
@@ -3,37 +3,10 @@
 import os
 from setuptools import setup, find_packages
 
-name = "Mikko Korpela"
-# I might be just a little bit too much afraid of those bots..
-address = name.lower().replace(" ", ".") + chr(64) + "gmail.com"
-
 setup(
-    name="robotframework-pabot",
     version="2.9.0",
-    description="Parallel test runner for Robot Framework",
-    long_description="A parallel executor for Robot Framework tests."
-    " With Pabot you can split one execution into multiple and save test execution time.",
-    author=name,
-    author_email=address,
-    url="https://pabot.org",
     project_urls={
         "Source": "https://github.com/mkorpela/pabot",
     },
     download_url="https://pypi.python.org/pypi/robotframework-pabot",
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Topic :: Software Development :: Testing",
-        "License :: OSI Approved :: Apache Software License",
-        "Development Status :: 5 - Production/Stable",
-        "Framework :: Robot Framework",
-    ],
-    license="Apache License, Version 2.0",
-    install_requires=[
-        "robotframework>=3.2",
-        "robotframework-stacktrace>=0.4.1",
-        "natsort>=8.2.0",
-    ],
-    python_requires=">=3.6",
 )
