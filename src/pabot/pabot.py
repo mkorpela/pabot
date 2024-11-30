7f34087ee79e086f0a195b90fa1ff55433c380a6
Mikko Korpela <mikko.korpela@gmail.com>
2024-11-30 14:14:15 +0200

    fix case where lang is empty

--- src/pabot/pabot.py
+++ src/pabot/pabot.py
@@ -1154,7 +1154,7 @@ def generate_suite_names_with_builder(outs_dir, datasources, options):
     # Note: first argument (included_suites) is deprecated from RobotFramework 6.1
     if ROBOT_VERSION >= "6.1":
         builder = TestSuiteBuilder(
-            included_extensions=settings.extension, rpa=settings.rpa, lang=opts["language"]
+            included_extensions=settings.extension, rpa=settings.rpa, lang=opts.get("language")
         )
     else:
         builder = TestSuiteBuilder(
