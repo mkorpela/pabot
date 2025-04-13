d2a942aeefdd1dd4806ae108e63dce19f7c501d5
Mikko Korpela <mikko.korpela@gmail.com>
2025-04-13 21:29:15 +0300

    update process

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
 cd ..
 git clone git@github.com:mkorpela/pabot.git pabotrelease
 cd pabotrelease
+pip install --upgrade pip build twine
 python -m build
+twine check dist/*
 twine upload -r robotframework-pabot dist/*.*
--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import os
+import sys
 
-setup()
+# Add src to path so that version can be imported
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
+from pabot import __version__
+
+setup(
+    name="robotframework-pabot",
+    version=__version__,
+)
