f1492e85aa8fcd7e7f2d45fe002bb9bb9d6faa40
Mikko Korpela <mikko.korpela@gmail.com>
2025-04-13 21:32:38 +0300

    PabotLib: import optional for version import

--- src/pabot/__init__.py
+++ src/pabot/__init__.py
@@ -1,5 +1,10 @@
 from __future__ import absolute_import
 
-from .pabotlib import PabotLib
+# Avoid import errors during setup/build
+try:
+    from .pabotlib import PabotLib
+    __all__ = ["PabotLib"]
+except ImportError:
+    pass
 
 __version__ = "4.2.0"
