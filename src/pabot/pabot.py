7b1ee20a95283108392d9a9fba7626c9bdfa939a
Joonas Kuisma <kuisma.joonas@gmail.com>
2025-06-08 08:52:16 +0300

    bugfix: pabot fails if datasource ends with a backslash
    - Fixes #167

--- src/pabot/pabot.py
+++ src/pabot/pabot.py
@@ -290,7 +290,8 @@ def _create_command_for_execution(caller_id, datasources, is_last, item, outs_di
             item.last_level,
             item.processes,
         )
-        + datasources
+        # If the datasource ends with a backslash, it is doubled to ensure correct handling of the escape character later on.
+        + [s + '\\' if not s.endswith('\\\\') and s.endswith('\\') else s for s in datasources]
     )
     return _mapOptionalQuote(cmd)
 
